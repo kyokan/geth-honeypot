5e209d148895ae3c39e1c170b58dee712013f413
Matthew Slipper <me@matthewslipper.com>
2018-07-06 14:34:25 -0700

    Add logger

---
+++ src/logger.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+const NEWLINES = /\n/ig;
+
+module.exports = function logger(logDir) {
+  const wStream = fs.createWriteStream(`${logDir}/req-log.log`);
+
+  return {
+    log(req) {
+      const payload = {
+        timestamp: Date.now(),
+        remoteIp: req.connection.remoteAddress,
+        headers: {},
+        url: req.url,
+        body: ''
+      };
+      
+      for (let i = 0; i < req.rawHeaders.length; i += 2) {
+        const j = i;
+        const k = i + 1;
+        
+        payload.headers[req.rawHeaders[j]] = req.rawHeaders[k];
+      }
+      
+      req.on('data', (chunk) => {
+        payload.body += chunk;
+      });
+
+      req.on('end', () => {
+        wStream.write(JSON.stringify(req).replace(NEWLINES, ' ') + '\n')
+      });
+    }
+  };
+};
